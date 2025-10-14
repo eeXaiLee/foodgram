9206394ab2fc0216fc11634d8f8d5deaec0aeb71
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-10-14 23:49:50 +0500

    feat: custom User admin; enable DRF Token & Djoser

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='email'
+    )
+    avatar = models.ImageField(
+        upload_to='users/',
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return self.email or self.username
