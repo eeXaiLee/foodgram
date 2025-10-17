1ef6163ca1e778d341b40fdcbf23a891461e171c
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-10-17 20:18:50 +0500

    feat: add Tag model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+        verbose_name='Тег',
+    )
+    slug = models.SlugField(
+        max_length=32,
+        unique=True,
+        verbose_name='Слаг',
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги',
+        ordering = ['id',]
+
+    def __str__(self) -> str:
+        return self.name
