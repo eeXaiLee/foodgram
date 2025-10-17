82b91d52afb94fab0d2a7c4c02e1a5a667a7dfda
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-10-17 22:38:16 +0500

    chore: register ingredients routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet, UserViewSet
+from .views import IngredientViewSet, TagViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
