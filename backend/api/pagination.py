79a324e12c1e5ddff5899e00b04a8db571ca6eab
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-10-17 20:13:28 +0500

    feat: enable global pagination (page/limit)

---
+++ backend/api/pagination.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class LimitPagination(PageNumberPagination):
+
+    page_query_param = 'page'
+    page_size_query_param = 'limit'
+    max_page_size = 100
