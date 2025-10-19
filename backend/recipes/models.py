9bad03d1ad95d747cb46658e976e1979f9200fe3
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-10-19 14:40:15 +0500

    chore: fix verbose_name and verbose_name_plural in Meta Recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -87,8 +87,8 @@ class Recipe(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Рецепт',
-        verbose_name_plural = 'Рецепты',
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
         ordering = ('-pub_date', 'id',)
         constraints = [
             models.UniqueConstraint(
