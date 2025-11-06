1f09c036650783237bd372cea37bfd3e00cc12d1
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2025-11-06 22:29:35 +0500

    refactor: fix DEFAULT_FILTER_BACKENDS in settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.LimitPagination',
     'PAGE_SIZE': 6,
-    'DEFAULT_FILTER_BACKEND': [
+    'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
     ],
